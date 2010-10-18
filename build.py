a400ab0439e032f4c61f56f00a5a9db1e44b18d2
Bogdan Opanchuk <mantihor@gmail.com>
2010-10-19 01:45:17 +1100

    edda, build: removed ledmac/ledpar, using tables and micropages instead

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ def sequence(contents, pair_limit=10):
 
 	res = u""
 
-	res += u"\\begin{pairs}\n\n"
+	#res += u"\\begin{pairs}\n\n"
 
 	for i, stanza in enumerate(contents):
 		number = stanza['number']
@@ -14,14 +14,14 @@ def sequence(contents, pair_limit=10):
 		translation = " \\\\\n".join(stanza['translation'])
 		comment = stanza['comment']
 
-		if (i + 1) % pair_limit == 0:
-			res += u"\\end{pairs}\n\n\\begin{pairs}\n\n"
+		#if (i + 1) % pair_limit == 0:
+		#	res += u"\\end{pairs}\n\n\\begin{pairs}\n\n"
 
 		res += u"\\mystanzapair % Stanza " + unicode(number) + u"\n" + \
 			(u"[" + comment + u"]\n" if comment is not None else u"") + \
 			u"{\n" + original + u"}\n{\n" + translation + u"}\n\n"
 
-	res += u"\\end{pairs}"
+	#res += u"\\end{pairs}"
 	return res
 
 voluspo = yaml.load(open("stanzas/voluspo.yaml"))
