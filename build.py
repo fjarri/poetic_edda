30743d5e2da431d685b7fe7bb99b47e2a86f326b
Bogdan Opanchuk <mantihor@gmail.com>
2010-10-20 20:41:24 +1100

    build: removed obsolete parts of sequence()

--- build.py
+++ build.py
@@ -2,11 +2,9 @@ import json
 import codecs
 
 
-def sequence(contents, pair_limit=10):
+def sequence(contents):
 
 	res = u""
-
-	#res += u"\\begin{pairs}\n\n"
 
 	for i, stanza in enumerate(contents):
 		number = stanza['number']
@@ -14,14 +12,10 @@ def sequence(contents, pair_limit=10):
 		translation = u" \\\\\n".join(stanza['translation'])
 		comment = u"\n".join(stanza['comment']) if stanza['comment'] is not None else None
 
-		#if (i + 1) % pair_limit == 0:
-		#	res += u"\\end{pairs}\n\n\\begin{pairs}\n\n"
-
 		res += u"\\mystanzapair % Stanza " + unicode(number) + u"\n" + \
 			(u"[" + comment + u"]\n" if comment is not None else u"") + \
 			u"{\n" + original + u"}\n{\n" + translation + u"}\n\n"
 
-	#res += u"\\end{pairs}"
 	return res
 
 voluspo = json.load(open("stanzas/voluspo.json"))
