6552c5ac6de90692de35c649a67fb9a35996239e
Bogdan Opanchuk <mantihor@gmail.com>
2011-01-11 12:08:08 +1100

    parse_intro: changed according to new chapter format

--- parse_intro.py
+++ parse_intro.py
@@ -1,12 +1,19 @@
-import json, codecs
-import re
+import re, codecs
 import sys
+from xml.etree.ElementTree import Element, ElementTree
+from detexify_json import makePretty
 
 f = codecs.open(sys.argv[1], 'r', 'utf-8')
-lines = f.read().split("\n")
+intro = f.read()
 f.close()
 
-c = [{"type": "text", "text": lines}]
+root = Element('chapter', icelandic_name="", english_name="", translation="")
 
-json.dump(c, codecs.open(sys.argv[2], 'w', 'utf-8'), ensure_ascii=False, indent=4)
+for block in intro.split(u'\n\n'):
+	e = Element('block', attrib={'class': 'text'})
+	e.text = block
+	root.append(e)
 
+tree = ElementTree(element=root)
+makePretty(tree.getroot())
+tree.write(open(sys.argv[2], mode='wb'), encoding='utf-8')
